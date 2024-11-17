79dade4936e44fdef5f5327a152fc5edaf235efb
srose096 <srose096@uottawa.ca>
2024-11-17 17:10:12 -0500

    lab9

---
+++ src/main/kotlin/seg3x02/tempconverterapi/WebSecurityConfig.kt
@@ -0,0 +1,63 @@
+package seg3x02.tempconverterapi
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig {
+
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { auth -> auth.anyRequest().authenticated() }
+            .formLogin {formLogin -> formLogin.loginPage("/login").permitAll() }
+            .logout(Customizer.withDefaults())
+        return http.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val user: UserDetails = User.withUsername("appuser")
+            .password(passwordEncoder().encode("userpassword"))
+            .roles("USER")
+            .build()
+        return InMemoryUserDetailsManager(user)
+    }
+
+    @Bean
+    fun webSecurityCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer { web: WebSecurity ->
+            web.ignoring()
+                .requestMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**","/vendor/**","/fonts/**") }
+    }
+
+    @Bean
+    fun passwordEncoder(): BCryptPasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun configureUsers(auth: AuthenticationManagerBuilder) {
+        auth.inMemoryAuthentication()
+            .withUser("user1")
+            .password(passwordEncoder().encode("pass1"))
+            .roles("USER")
+            .and()
+            .withUser("user2")
+            .password(passwordEncoder().encode("pass2"))
+            .roles("USER")
+    }
+}
